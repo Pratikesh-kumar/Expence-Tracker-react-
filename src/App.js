78bd990bf77d0d8d0fc887cd7222bd70d398ac41
Pratikesh-kumar <pratikeshkumar96@gmail.com>
2023-03-30 15:14:07 +0530

    signIn and welcome page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+
+import React from 'react';
+import { Route,Redirect} from 'react-router-dom';
 import SignUp from './components/Pages/SignUp';
+import SignIn from './components/Pages/SignIn';
+import { LoginContextProvider } from './components/Context/LoginContext';
+
+import Welcome from './components/Pages/Welcome';
 import './App.css';
 
 function App() {
   return (
-   <>
-   <SignUp/>
-   </>
+    <React.Fragment>
+    <LoginContextProvider>
+      {/* <Route path="*">
+        <Redirect to="/signUp" />
+      </Route>
+
+      <Route path="/signUp">
+        <SignUp />
+      </Route> */}
+
+      <Route path="/signIn">
+        <SignIn />
+      </Route>
+
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+    </LoginContextProvider>
+  </React.Fragment>
+
   );
 }
 
